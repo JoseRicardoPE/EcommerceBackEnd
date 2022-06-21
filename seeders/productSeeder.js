9ae1ec984eed3be101829ed210e7623d13a54b27
Triana Misa <trianamisa@gmail.com>
2022-06-21 12:19:50 -0300

    Update productSeeder.js

--- seeders/productSeeder.js
+++ seeders/productSeeder.js
@@ -9,7 +9,7 @@ module.exports = async () => {
       path: "https://cdn.shopify.com/s/files/1/0435/8216/1057/products/jv__0019_Layer1_1080x.jpg?v=1653585217",
       price: 50,
       stock: 5,
-      tag_id: 1,
+      tagId: 1,
       isOutsiding: true,
     },
     {
@@ -19,6 +19,7 @@ module.exports = async () => {
       path: "https://cdn.shopify.com/s/files/1/0435/8216/1057/products/jv__0018_Layer2_720x.jpg?v=1653585071",
       price: 50,
       stock: 5,
+      tagId: 1,
       isOutsiding: true,
     },
     {
@@ -28,7 +29,7 @@ module.exports = async () => {
       path: "https://cdn.shopify.com/s/files/1/0435/8216/1057/products/jv__0017_Layer3_720x.jpg?v=1653585155",
       price: 50,
       stock: 5,
-      tag_id: 1,
+      tagId: 1,
       isOutsiding: true,
     },
     {
@@ -38,7 +39,7 @@ module.exports = async () => {
       path: "https://cdn.shopify.com/s/files/1/0435/8216/1057/products/jv__0016_Layer4_720x.jpg?v=1653585179",
       price: 50,
       stock: 5,
-      tag_id: 1,
+      tagId: 1,
       isOutsiding: true,
     },
     {
@@ -48,7 +49,7 @@ module.exports = async () => {
       path: "https://cdn.shopify.com/s/files/1/0435/8216/1057/products/jv__0015_Layer5_720x.jpg?v=1653585115",
       price: 50,
       stock: 5,
-      tag_id: 1,
+      tagId: 1,
       isOutsiding: true,
     },
   ];
