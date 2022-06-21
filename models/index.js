d0c9b1aa6062e8c476b8303ed7ae992ebf577497
manugarderes <69734460+manugarderes@users.noreply.github.com>
2022-06-21 11:53:11 -0300

    Relations

--- models/index.js
+++ models/index.js
@@ -7,5 +7,33 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 })
 
 const User = require("./User")(sequelize, Model, DataTypes)
+const Address = require("./Address")(sequelize, Model, DataTypes)
+const Order_Product = require("./Order_Product")(sequelize, Model, DataTypes)
+const Order = require("./Order")(sequelize, Model, DataTypes)
+const Payment = require("./Payment")(sequelize, Model, DataTypes)
+const Product = require("./Product")(sequelize, Model, DataTypes)
+const ProductImages = require("./ProductImages")(sequelize, Model, DataTypes)
+const Tag = require("./Tag")(sequelize, Model, DataTypes)
 
-module.exports = {sequelize, User}
+User.hasMany(Address)
+Address.belongsTo(User)
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
+Payment.hasMany(Order)
+Order.belongsTo(Payment)
+
+Product.hasMany(ProductImages)
+ProductImages.belongsTo(Product)
+
+Tag.hasMany(Product)
+Product.belongsTo(Tag)
+
+Order.hasMany(Order_Product)
+Order_Product.belongsTo(Order)
+
+Product.hasMany(Order_Product)
+Order_Product.belongsTo(Product)
+
+module.exports = {sequelize, User, Address, Order_Product, Order, Payment, Product, ProductImages, Tag}
