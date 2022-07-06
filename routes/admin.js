89bfa051bf746257d11e6e5f7247542350810f8c
Triana Misa <trianamisa@gmail.com>
2022-07-06 12:50:09 -0300

    Revert "Update admin.js"
    This reverts commit 0dc17b99eb33fa22e14039e9fa52f1a9a75af2d8.

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const checkJwt = require("../middleware/checkToken");
+const checkJwt = require("../middleware/checkToken")
 const {
   createProduct,
   updateProduct,
@@ -23,32 +23,31 @@ const {
   orderDelete,
 } = require("../controllers/adminController");
 
-router.get("/order/show", showAllOrders);
-router.get("/order/show/:id", showOrder);
+router.use("/", checkJwt)
 
-router.use("/", checkJwt);
+router.post("/c/product",createProduct);
+router.post("/u/product/:id",updateProduct);
+router.get("/d/product/:id",deleteProduct);
 
-router.post("/c/product", createProduct);
-router.post("/u/product/:id", updateProduct);
-router.get("/d/product/:id", deleteProduct);
+router.post("/u/user",updateUser);
+router.get("/d/user/:id",deleteUser);
 
-router.post("/u/user", updateUser);
-router.get("/d/user/:id", deleteUser);
-
-router.post("/c/image", createImage);
-router.post("/u/image/:id", updateImage);
-router.get("/d/image/:id", deleteImage);
+router.post("/c/image",createImage);
+router.post("/u/image/:id",updateImage);
+router.get("/d/image/:id",deleteImage);
 
 router.post("/c/tag", createTag);
-router.post("/u/tag/:id", updateTag);
-router.get("/d/tag/:id", deleteTag);
+router.post("/u/tag/:id",updateTag);
+router.get("/d/tag/:id",deleteTag);
 
-router.post("/c/payment", createPayment);
-router.post("/u/payment/:id", updatePayment);
-router.get("/d/payment/:id", deletePayment);
+router.post("/c/payment",createPayment);
+router.post("/u/payment/:id",updatePayment);
+router.get("/d/payment/:id",deletePayment);
 
-router.post("/c/order", orderCreate);
-router.post("/u/order/:id", orderUpdate);
-router.post("/d/order/:id", orderDelete);
+router.get("/order/show",showAllOrders);
+router.post("/c/order",orderCreate);
+router.get("/order/show/:id",showOrder);
+router.post("/u/order/:id",orderUpdate);
+router.post("/d/order/:id",orderDelete);
 
 module.exports = router;
